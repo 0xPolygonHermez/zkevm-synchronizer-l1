65f97ca29c85587f7e87d822421aeded9897b2e8
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-05-08 07:30:44 +0200

    move mock generation to 'go generate'

--- synchronizer/l1_check_block/types.go
+++ synchronizer/l1_check_block/types.go
@@ -1,5 +1,8 @@
 package l1_check_block
 
+//go:generate bash -c "rm -Rf mocks"
+//go:generate mockery --all --case snake --dir . --output ./mocks --outpkg mock_l1_check_block  --disable-version-string --with-expecter
+
 import "github.com/0xPolygonHermez/zkevm-synchronizer-l1/state/entities"
 
 type L1Block = entities.L1Block
