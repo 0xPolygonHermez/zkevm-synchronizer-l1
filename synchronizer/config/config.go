856865a58bc5a21ae92da96e1c637bb38969331f
Xavier Romero <xavier@polygon.technology>
2024-06-07 21:35:39 +0200

    lastest -> latest

--- synchronizer/config/config.go
+++ synchronizer/config/config.go
@@ -16,13 +16,13 @@ type Config struct {
 	// if it's zero it finds the etrog upgrade block
 	GenesisBlockNumber uint64 `mapstructure:"GenesisBlockNumber"`
 
-	// SyncBlockProtection specify the synchronization block (lastest, finalized, pending or safe) also can have an offset
+	// SyncBlockProtection specify the synchronization block (latest, finalized, pending or safe) also can have an offset
 	// example: safe/-10, then the safe block ten blocks before the safe block
-	SyncBlockProtection string `jsonschema:"enum=lastest,enum=safe, enum=pending, enum=finalized" mapstructure:"SyncBlockProtection"`
-	// SyncBlockFinalized specify what block is considered finalized (lastest, finalized, pending or safe) also can have an offset
+	SyncBlockProtection string `jsonschema:"enum=latest,enum=safe, enum=pending, enum=finalized" mapstructure:"SyncBlockProtection"`
+	// SyncBlockFinalized specify what block is considered finalized (latest, finalized, pending or safe) also can have an offset
 	// example: safe/-10, then the safe block ten blocks before the finalized block
 	// if not set assuming 'finalized'
-	SyncBlockFinalized string `jsonschema:"enum=lastest,enum=safe, enum=pending, enum=finalized" mapstructure:"SyncBlockFinalized"`
+	SyncBlockFinalized string `jsonschema:"enum=latest,enum=safe, enum=pending, enum=finalized" mapstructure:"SyncBlockFinalized"`
 
 	// OverrideStorageCheck is a flag to override the storage check
 	// take in account that without that check you can merge data from different rollups or differents L1 networks
