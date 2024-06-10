15cd821424c7101c4ef15cff44a83af2aae8fc10
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-06-10 13:30:35 +0200

    fix mapping of config values

---
+++ config/default_test.go
@@ -0,0 +1,53 @@
+package config_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/0xPolygonHermez/zkevm-synchronizer-l1/config"
+	"github.com/0xPolygonHermez/zkevm-synchronizer-l1/config/types"
+	"github.com/0xPolygonHermez/zkevm-synchronizer-l1/etherman"
+	"github.com/0xPolygonHermez/zkevm-synchronizer-l1/log"
+	storage "github.com/0xPolygonHermez/zkevm-synchronizer-l1/state/storage"
+	syncconfig "github.com/0xPolygonHermez/zkevm-synchronizer-l1/synchronizer/config"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDefault(t *testing.T) {
+	expectedCfg := &config.Config{
+		Log: log.Config{
+			Environment: "development",
+			Level:       "info",
+			Outputs:     []string{"stderr"},
+		},
+		DB: storage.Config{
+			Name:     "sync",
+			User:     "test_user",
+			Password: "test_password",
+			Host:     "localhost",
+			Port:     "5436",
+			MaxConns: 10,
+		},
+		Synchronizer: syncconfig.Config{
+			SyncInterval:         types.Duration{time.Second * 10},
+			SyncChunkSize:        500,
+			GenesisBlockNumber:   0,
+			SyncUpToBlock:        "latest",
+			BlockFinality:        "finalized",
+			OverrideStorageCheck: false,
+		},
+		Etherman: etherman.Config{
+			L1URL: "http://localhost:8545",
+			Contracts: etherman.ContractConfig{
+				GlobalExitRootManagerAddr: common.HexToAddress("0x2968D6d736178f8FE7393CC33C87f29D9C287e78"),
+				RollupManagerAddr:         common.HexToAddress("0xE2EF6215aDc132Df6913C8DD16487aBF118d1764"),
+				ZkEVMAddr:                 common.HexToAddress("0x89BA0Ed947a88fe43c22Ae305C0713eC8a7Eb361"),
+			},
+		},
+	}
+	cfg, err := config.Default()
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
+	require.Equal(t, expectedCfg, cfg)
+}
--- synchronizer/config/config.go
+++ synchronizer/config/config.go
@@ -18,11 +18,11 @@ type Config struct {
 
 	// SyncUpToBlock specifies the synchronization highest block to sync (latest, finalized, pending or safe). also can have an offset
 	// example: safe/-10, then the safe block ten blocks before the safe block
-	SyncUpToBlock string `jsonschema:"enum=latest,enum=safe, enum=pending, enum=finalized" mapstructure:"SyncBlockProtection"`
+	SyncUpToBlock string `jsonschema:"enum=latest,enum=safe, enum=pending, enum=finalized" mapstructure:"SyncUpToBlock"`
 	// BlockFinality indicates which block is considered finalized (latest, finalized, pending or safe) also can have an offset
 	// example: safe/-10, then the safe block ten blocks before the finalized block
 	// if not set assuming 'finalized'
-	BlockFinality string `jsonschema:"enum=latest,enum=safe, enum=pending, enum=finalized" mapstructure:"SyncBlockFinalized"`
+	BlockFinality string `jsonschema:"enum=latest,enum=safe, enum=pending, enum=finalized" mapstructure:"BlockFinality"`
 
 	// OverrideStorageCheck is a flag to override the storage check
 	// take in account that without that check you can merge data from different rollups or differents L1 networks
