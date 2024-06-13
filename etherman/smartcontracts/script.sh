bd9dfdc242c2c26e50f87e1931687a0c9c191238
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-06-13 21:10:01 +0200

    Revert "new contract DA and update etrog to validium one (that extents zkevm, so it's compatible)"
    This reverts commit 56fcbfcbdf9d2d077326d3ff37550829b08d6748.

--- etherman/smartcontracts/script.sh
+++ etherman/smartcontracts/script.sh
@@ -8,12 +8,6 @@ gen() {
     abigen --bin bin/${package}.bin --abi abi/${package}.abi --pkg=${package} --out=${package}/${package}.go
 }
 
-genNoBin() {
-    local package=$1
-
-    abigen --abi abi/${package}.abi --pkg=${package} --out=${package}/${package}.go
-}
-
 gen oldpolygonzkevmglobalexitroot
 gen oldpolygonzkevmbridge
 gen oldpolygonzkevm
@@ -25,6 +19,4 @@ gen polygonzkevmglobalexitroot
 gen polygonrollupmanager
 gen mockpolygonrollupmanager
 gen mockverifier
-gen polygondatacommittee
-genNoBin dataavailabilityprotocol
 gen proxy
