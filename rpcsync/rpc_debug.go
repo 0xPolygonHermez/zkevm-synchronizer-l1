35472b1e709076f086499a1be50982b92ea8adc9
joanestebanr <129153821+joanestebanr@users.noreply.github.com>
2024-08-28 14:34:06 +0200

    add comments

--- rpcsync/rpc_debug.go
+++ rpcsync/rpc_debug.go
@@ -9,6 +9,25 @@ import (
 	jRPC "github.com/0xPolygon/cdk-rpc/rpc"
 )
 
+
+/*
+To activate this you need the build tag `debug`:
+- Example of launch.json entry
+"name": "run CARDONA",
+            "type": "go",
+            "request": "launch",
+            "mode": "auto",
+            "buildFlags": "-tags 'debug'",
+            "program": "cmd/main.go",
+		
+
+This RPC is deployed on port 1025:
+- To execute rollbackBatches: lastBatch, accInputHash, l1BlockNumber
+curl -X POST http://localhost:1025/ -H "Con -application/json" -d '{"method":"debug_rollbackBatches", "params":[53992, "0x1234", 5159957], "id":1}'
+- To executge forceReorg: firstL1BlockNumberToKeep
+curl -X POST http://localhost:1025/ -H "Con -application/json" -d '{"method":"debug_forceReorg", "params":[5159956], "id":1}'
+
+
 func StartRPC(state interface{}) {
 	cfg := jRPC.Config{
 		Port:                      1025,
