b2059507876f8186a0f39338b5e73a7ed23b0bd2
Joan Esteban <129153821+joanestebanr@users.noreply.github.com>
2024-09-25 13:23:24 +0200

    Add check to DB configuration (#118)

--- state/storage/sqlstorage/config.go
+++ state/storage/sqlstorage/config.go
@@ -1,7 +1,26 @@
 package sqlstorage
 
+import "fmt"
+
 type Config struct {
 	// Name of the database
 	DriverName string `mapstructure:"DriverName"`
 	DataSource string `mapstructure:"DataSource"`
+}
+
+func (c *Config) String() string {
+	return fmt.Sprintf("DriverName=%s DataSource=%s", c.DriverName, c.DataSource)
+}
+
+func (c *Config) SanityCheck() error {
+	if c.DriverName == "" {
+		return fmt.Errorf("DriverName is required")
+	}
+	if c.DataSource == "" {
+		return fmt.Errorf("DataSource is required")
+	}
+	if c.DriverName != SqliteDriverName {
+		return fmt.Errorf("DriverName not supported: %s, only supports: %s", c.DriverName, SqliteDriverName)
+	}
+	return nil
 }
